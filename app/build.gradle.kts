f8ccd41d9d1e297ee0a4ecc380aa642995c138f9
carlosttorres33 <kisamehatake33@comunidad.unam.mx>
2024-04-24 11:58:26 -0600

    Crear partida (Enviar datos de la partida a Realtime Database)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,16 +48,16 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.4.1"
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-//    kotlin{
-//        jvmToolchain(8)
-//    }
+    kotlin{
+        jvmToolchain(8)
+    }
 }
 
 dependencies {
--- app/src/main/java/com/carlostorres/tictactoe/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/carlostorres/tictactoe/ui/home/HomeViewModel.kt
@@ -2,6 +2,8 @@ package com.carlostorres.tictactoe.ui.home
 
 import androidx.lifecycle.ViewModel
 import com.carlostorres.tictactoe.data.network.FirebaseService
+import com.carlostorres.tictactoe.data.network.model.GameData
+import com.carlostorres.tictactoe.data.network.model.PlayerData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -11,6 +13,23 @@ class HomeViewModel @Inject constructor(
 ) : ViewModel() {
 
     fun onCreateGame() {
+
+        firebaseService.createGame(createNewGame())
+
+    }
+
+    private fun createNewGame(): GameData{
+
+        val currentPlayer = PlayerData(playerType = 1)
+
+        return GameData(
+
+            board = List(9){ 0 },
+            player1 = currentPlayer,
+            playerTurn = currentPlayer,
+            player2 = null
+
+        )
 
     }
 
